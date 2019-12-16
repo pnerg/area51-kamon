37a9b6b7051b94c9d18fc9b29d7e18c1e1fc65ed
Peter Nerg <peter.nerg@gmail.com>
2019-12-16 14:11:15 +0100

    configured custom span name generators

--- common/src/main/scala/org/dmonix/area51/kamon/CustomNameGenerator.scala
+++ common/src/main/scala/org/dmonix/area51/kamon/CustomNameGenerator.scala
@@ -1,14 +1,16 @@
 package org.dmonix.area51.kamon
 
+import com.typesafe.scalalogging.LazyLogging
 import kamon.instrumentation.http.HttpMessage.Request
 import kamon.instrumentation.http.HttpOperationNameGenerator
 
 /**
   * @author Peter Nerg
   */
-class CustomNameGenerator extends HttpOperationNameGenerator {
+class CustomNameGenerator extends HttpOperationNameGenerator with LazyLogging {
+  logger.info("Creating CustomNameGenerator")
   override def name(request: Request): Option[String] = {
-    println("Name generator : "+request.path)
+    logger.info("Name generator : "+request.path)
     Some(request.path)
   }
 }
