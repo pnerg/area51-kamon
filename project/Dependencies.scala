d9938699e0604e7ddb0758b4141fb33212b7c407
Peter Nerg <peter.nerg@gmail.com>
2021-03-28 02:26:34 +0000

    other: update scala-logging from 3.9.2 to 3.9.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies extends AutoPlugin {
 
   object autoImport {
 
-    val `scala-logging` = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
+    val `scala-logging` = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.3"
 
     val `akka-actor` = "com.typesafe.akka" %% "akka-actor" % "2.6.13"
     val `akka-slf4j` = "com.typesafe.akka" %% "akka-slf4j" % `akka-actor`.revision
